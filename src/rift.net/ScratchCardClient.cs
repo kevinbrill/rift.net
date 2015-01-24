102499c0e67463d11528b486367cadbf94351b45
Kevin Brill <kevin.brill@gmail.com>
2015-01-24 17:04:14 -0500

    Added test html docs

--- src/rift.net/ScratchCardClient.cs
+++ src/rift.net/ScratchCardClient.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using rift.net.Models.Games;
 using RestSharp;
 using rift.net.rest;
+using HtmlAgilityPack;
 
 namespace rift.net
 {
@@ -52,10 +53,20 @@ namespace rift.net
 
 			// Create a new request based on the game's URL.  The base URL already includes
 			//  /chatservice, so go ahead and remove it.
-			var request = CreateRequest(selectedGame.Url.Replace ("/chatservice", "/"));
+			var request = CreateRequest (selectedGame.Url.Replace ("/chatservice", "/"));
 			request.AddQueryParameter ("characterId", characterId);
 
 			var response = Client.Execute (request);
+
+			// Another option may be to parse the html returned by the call.
+			//  There is a <div> with an ID of "reward-layer" and a span
+			//  with a class of "reward-text".  In the case of not winning, it is the following:
+			// 
+			// <div id="reward-layer">
+			//		<span class="reward-text">Sorry, you did not win. Please try again later.</span>
+			// </div> 
+			//
+			// Could we parse that the determine the result?
 
 			// Winner?
 			//return '/chatservice/scratch/redeem?game=02a07cd8-f08b-4931-9488-3b44370ad2b3';
@@ -65,6 +76,14 @@ namespace rift.net
 			if ((response.ResponseStatus != RestSharp.ResponseStatus.Completed) ||
 			   (response.StatusCode != System.Net.HttpStatusCode.OK)) {
 				throw new Exception ("An error occurred calling the service", response.ErrorException);
+			}
+
+			// Create and load an html document from the response
+			var doc = new HtmlDocument ();
+			try {
+				doc.LoadHtml (response.Content);
+			} catch (Exception ex) {
+				
 			}
 
 			if (IsGameAWinner (response.Content)) {
@@ -88,31 +107,27 @@ namespace rift.net
 			return indexOfReplay >= 0;
 		}
 
-		private Uri GetWinningUrl( string content )
+		private string GetWinningUrl( string content )
 		{
 			var indexOfRedeem = content.IndexOf ("/chatservice/scratch/redeem?game=");
 			var indexOfSemi = content.IndexOf (";", indexOfRedeem);
 
-			var uri = new Uri (content.Substring (indexOfRedeem, indexOfSemi - indexOfRedeem - 1));
-
-			return uri;
+			return content.Substring (indexOfRedeem, indexOfSemi - indexOfRedeem - 1);
 		}
 
-		private Uri GetReplayUrl( string content )
+		private string GetReplayUrl( string content )
 		{
 			var indexOfReplay = content.IndexOf ("/chatservice/scratch/");
 			var indexOfSemi = content.IndexOf (";", indexOfReplay);
 
-			var uri = new Uri (content.Substring (indexOfReplay, indexOfSemi - indexOfReplay - 1));
-
-			return uri;
+			return content.Substring (indexOfReplay, indexOfSemi - indexOfReplay - 1);
 		}	
 
 		private void ClaimPrize( string content )
 		{
 			var url = GetWinningUrl (content);
 
-			var request = CreateRequest (url.ToString(), Method.GET);
+			var request = CreateRequest (url, Method.GET);
 
 			var response = Client.Execute(request);
 
@@ -123,7 +138,7 @@ namespace rift.net
 		{
 			var url = GetReplayUrl (content);
 
-			var request = CreateRequest (url.ToString(), Method.GET);
+			var request = CreateRequest (url, Method.GET);
 
 			var response = Client.Execute(request);
 

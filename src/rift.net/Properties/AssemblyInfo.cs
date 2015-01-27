a132394629ffc02c731b99444d9a50b4b255b2f3
Kevin Brill <kevin.brill@gmail.com>
2015-01-27 10:29:15 -0500

    Added nuget spec

--- src/rift.net/Properties/AssemblyInfo.cs
+++ src/rift.net/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("rift.net")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("A .net wrapper for Trion World's MMO 'Rift' REST API.")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("Kevin Brill <kevin.brill@gmail.com>")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("kevin")]
 [assembly: AssemblyTrademark ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.9.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.

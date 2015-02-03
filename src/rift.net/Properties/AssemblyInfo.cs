96c8200393ae6921fa5aec3a9ef8fefc9aef0d96
Kevin Brill <kevin.brill@gmail.com>
2015-02-03 10:52:12 -0500

    Added NuGet package restore

--- src/rift.net/Properties/AssemblyInfo.cs
+++ src/rift.net/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle ("rift.net")]
 [assembly: AssemblyDescription ("A .net wrapper for Trion World's MMO 'Rift' REST API.")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Kevin Brill <kevin.brill@gmail.com>")]
+[assembly: AssemblyCompany ("Kevin Brill")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("kevin")]
 [assembly: AssemblyTrademark ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.9.*")]
+[assembly: AssemblyVersion ("0.9.6.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.

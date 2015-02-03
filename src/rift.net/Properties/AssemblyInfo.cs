12d2bdd1b82a261217c17d676fda27740eda4516
Kevin Brill <kevin.brill@gmail.com>
2015-02-03 11:23:17 -0500

    Updated version number because .net is silly

--- src/rift.net/Properties/AssemblyInfo.cs
+++ src/rift.net/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Kevin Brill")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("kevin")]
+[assembly: AssemblyCopyright ("Copyright 2015 Kevin Brill")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.9.6.*")]
+[assembly: AssemblyVersion ("0.9.6000.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.

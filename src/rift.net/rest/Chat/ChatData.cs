af5b3d753e50b49becf7643f3d6dec0df325e88a
Kevin Brill <kevin.brill@gmail.com>
2015-02-04 14:29:46 -0500

    Looks like messages are being received.  Built out a good portion of the chat infrastructure

--- src/rift.net/rest/Chat/ChatData.cs
+++ src/rift.net/rest/Chat/ChatData.cs
@@ -1,23 +1,46 @@
 ﻿using System;
-using rift.net.rest.Chat;
 
 namespace rift.net
 {
-    public class ChatData
-    {
-        public string messageId { get; set; }
+	public class ChatData
+	{
+		/*
+		 * "messageId":"300811425",
+		 * "recipientId":218846794414042822,
+		 * "messageTime":1385959510,
+		 * "senderId":240098155572218997,
+		 * "senderName":"Thamin",
+		 * "message":"I\u0026apos;d talk more but we\u0026apos;re powering some folks through a dungeon. Once you have access to Rift member\u0026apos;s section, look for my \u0026quot;pocket guide to grievance/rift\u0026quot;, answers a lot of questions"
+		 */
+		public string messageId {
+			get;
+			set;
+		}
 
-        public long recipientId { get; set; }
+		public long recipientId {
+			get;
+			set;
+		}
 
-        public long messageTime { get; set; }
+		public long messageTime {
+			get;
+			set;
+		}
 
-        public long senderId { get; set; }
+		public long senderId {
+			get;
+			set;
+		}
 
-        public string senderName { get; set; }
+		public string senderName {
+			get;
+			set;
+		}
 
-        public string message { get; set; }
-
-        public ChatChannel ChatChannel { get; set; }
-    }
+		public string message {
+			get;
+			set;
+		}
+	}
 }
 
